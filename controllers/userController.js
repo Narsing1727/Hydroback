b7768752536db9639eda8227b65184a03002f783
NARSING SHARMA <newtongaming36gmail.com>
2025-10-26 10:13:19 +0530

    otp change5

--- controllers/userController.js
+++ controllers/userController.js
@@ -272,7 +272,7 @@ exports.SendOTP = async (req, res) => {
      
        const resend = new Resend(process.env.RESEND_API_KEY);
 
-    await resend.emails.send({
+    const response = await resend.emails.send({
      from: "onboarding@resend.dev",
       to: email,
       subject: "HydroSphere - Your OTP Code",
