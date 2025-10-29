3c6b926b0df7eb03c9621c8af5f268283a99c072
NARSING SHARMA <newtongaming36gmail.com>
2025-10-29 20:30:57 +0530

    test13

--- controllers/predictionController.js
+++ controllers/predictionController.js
@@ -5,7 +5,7 @@ const path = require("path");
  exports.model = async (req, res) => {
   try {
     const inputData = req.body;
-    const response = await fetch("http://127.0.0.1:8000/predict", {
+    const response = await fetch("https://hydromodel.onrender.com/predict", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ data: inputData }),
